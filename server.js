20137e3ce8a030caa82fd86d986ab55131375948
elsaaeid <saidsadaoy@gmail.com>
2024-06-05 16:27:13 -0700

    deploy changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
-const cartRoute = require("./routes/cartRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
@@ -46,7 +45,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/contactus", contactRoute);
-app.use("/api/cart", cartRoute);
 
 // Routes
 app.get("*", (req, res) => {
